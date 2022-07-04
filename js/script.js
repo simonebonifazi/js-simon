4b46e8e8a7959d45011166c08124712e09a79257
simonebonifazi <simonbonifraz@gmail.com>
2022-07-04 21:00:28 +0200

    added-timer-with-countdown-boosted-for-trials

--- js/script.js
+++ js/script.js
@@ -16,12 +16,8 @@ subito dopo, generiamo 5 numeri casuali tramite una funzione
 creiamo un timer di trenta secondi che si avvii all'inizio della pagina, perchè no, tramite funzione
 piazzare alert e prompt alla fine con gap, affinchè non avremo problemi di tempistiche con questi; 
 */
-//dichiaro variabili globali
-const timer = document.querySelector('countdown');
-const numbersList = document.getElementById('numbers');
 
-//funzione generatrice 5 numeri causuali 
-/**
+/**funzione generatrice numeri causuali
  * 
  * @param {number} min valore minimo del numero
  * @param {number} max valore massimo del numero
@@ -42,7 +38,40 @@ function getTotRandomNumbers(min, max, total) {
     }
     return randomNumber;
 }
+/** funzione che imposta il timer a 30 secondi
+ * 
+ */
+const myInterval = () => {
+    //stampo nel DOM i secondi
+    timer.innerText = seconds;
+    //decremento
+    --seconds
+    //fermo la funzione allo scadere del countdown
+    if (seconds === 0) {
+        clearInterval(countDown);
+        timer.classList.add('hidden')
+    }
+}
+//dichiaro variabili globali
+const timer = document.getElementById('countdown');
+const numbersList = document.getElementById('numbers');
+//timer 30 secondi
+let seconds = 30;
 
-let myNumbers = getTotRandomNumbers(1, 100, 100).sort();
+//invoco la funzione
+let myNumbers = getTotRandomNumbers(1, 100, 5);
 console.log(myNumbers)
+//stampo in DOM tramite ciclo for
+for (i = 0; i < myNumbers.length; i++) {
+    //creo gli elementi da inserire
+    const listItems = document.createElement('li');
+    //ci aggiunngo i singoli valori del mio array di numeri casuali
+    listItems.append(myNumbers[i]);
+    console.log(myNumbers[i])
+    //li inserisco nel DOM
+    numbersList.append(listItems);
+    console.log(listItems)
+}
 
+//stampo in console i trenta secondi che decrementeranno 1000ms alla volta
+const countDown = setInterval(myInterval, 100)
