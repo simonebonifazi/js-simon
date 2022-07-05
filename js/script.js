04041ea42ad67bb0d069e1997f55c9f73b550f2d
simonebonifazi <simonbonifraz@gmail.com>
2022-07-05 02:07:49 +0200

    to-fix-result

--- js/script.js
+++ js/script.js
@@ -62,7 +62,7 @@ const myInterval = () => {
 function promptLag() {
 
     for (i = 0; i < 5; i++) {
-        userAnswer.push(parseInt(prompt('inserisci qui, in ordine, i numeri che hai visto!')));
+        userAnswer.push(parseInt(prompt(`inserisci qui, il ${i + 1}° numero che hai memorizzato!`)));
     }
     console.log(userAnswer)
     //verifico se ha indovinato e quante, nel caso
@@ -70,18 +70,39 @@ function promptLag() {
 
 }
 
+/**
+ * funzione per stabilire quante domande ha indovinato 
+ * @param number risposte dell'utente
+ * @param number numeri casuali generati dal software
+ * @returns numero delle volte in cui sono uguali le due 
+ */
+function results(userNumbers, cpuNumbers) {
+    let result = 0;
+    userNumbers = 5
+    cpuNumbers = 5
+    // per ogni numer dell'array delle risposte che corrisponde ad un numero del gioco, aggiungo un punto
+    for (let i = 0; i < userNumbers; i++) {
+        for (let j = 0; j < cpuNumbers; j++) {
+            if ([i] === [j]) {
+                result++
+            }
+        }
+    }
+    return alert(result);
+
+}
 //******************** variabili globali
 const timer = document.getElementById('countdown');
 const numbersList = document.getElementById('numbers');
 const startGame = document.getElementById('start-game');
 const countDown = '';
 const delayedQuestion = '';
+const gameResults = 0;
 //preparao un array per contenere le risposte dell'utente
 const userAnswer = [];
-
-
 //timer 30 secondi
 let seconds = 30;
+
 /*********************************event listener al click */
 startGame.addEventListener('click', function () {
 
@@ -109,6 +130,8 @@ startGame.addEventListener('click', function () {
     //chiedo i 5 numeri all'utente
     const delayedQuestion = setTimeout(promptLag, 3050);
 
-    //al
+    //verifico tramite funzione se e quante domande ha indovinato
+    // const gameResults = results(myNumbers, userAnswer)
+    const delayedResults = setTimeout(results, 4050)
 })
 
